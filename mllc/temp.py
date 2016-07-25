801ca06b109cf8c0ad80dffa268bd648881c3c5f
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-07-25 12:52:18 +0300

    added PCA, GBC

--- mllc/temp.py
+++ mllc/temp.py
@@ -55,8 +55,11 @@ y_1 = np.ones(len(X_1))
 y = np.hstack((y_0, y_1))
 
 # Split data to train & test samples and fit scaler & imputer on training sample
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+from sklearn.cross_validation import StratifiedShuffleSplit
+skf = StratifiedShuffleSplit(y, n_iter=1, test_size=0.2)
+for train_index, test_index in skf:
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
 print("Train data: ", len(y_train),
       float(np.count_nonzero(y_train)) / len(y_train))
 print("Test data: ", len(y_test),
@@ -170,3 +173,178 @@ for i, (train_index, test_index) in enumerate(skf):
     print_cm_summary(cm)
 
 
+# # Finally fit GBC
+print("Using GBC")
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.grid_search import GridSearchCV
+import sklearn.pipeline
+
+skf = StratifiedShuffleSplit(y, n_iter=n_cv_iter, test_size=1./n_cv_iter)
+imp = Imputer(missing_values='NaN', strategy='median', axis=0, verbose=2)
+steps = [('imputation', imp), ('scaling', StandardScaler()),
+         ('classification',
+          GradientBoostingClassifier(verbose=0, n_estimators=1000))]
+pipeline = sklearn.pipeline.Pipeline(steps)
+param_grid = {'classification__learning_rate': [0.3, 0.1, 0.05, 0.01],
+              'classification__max_depth': [2, 3, 4, 5],
+              'classification__min_samples_leaf': [2, 4, 8, 16],
+              'classification__max_features': [1.0, 0.5, 0.2, 0.1]}
+
+gs_cv = GridSearchCV(pipeline, param_grid, cv=skf, n_jobs=1).fit(X, y)
+print("The best parameters are %s with a score of %0.2f"
+      % (gs_cv.best_params_, gs_cv.best_score_))
+
+# Final fit with best parameters
+clf_final = make_pipeline(imp, StandardScaler(),
+                          GradientBoostingClassifier(n_estimators=1000,
+                                                     **gs_cv.best_params_))
+skf = StratifiedShuffleSplit(y, n_iter=1, test_size=0.2)
+for train_index, test_index in skf:
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+clf_final.fit(X_train, y_train)
+print "Feature" \
+      " importance : {}".format(clf_final.named_steps['gradientboostingclassifier'].feature_importances_ /
+                                np.sum(clf_final.named_steps['gradientboostingclassifiers'].feature_importances_))
+y_pred = clf_final.predict(X_test)
+report = sklearn.metrics.classification_report(y_test, y_pred)
+print(report)
+cm = confusion_matrix(y_test, y_pred)
+cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+print_cm_summary(cm)
+
+
+# Using imputation, scaling, Randomized PCA & Random Forest
+from sklearn import decomposition, pipeline
+imp = Imputer(missing_values='NaN', strategy='median', axis=0, verbose=2)
+pca = decomposition.RandomizedPCA()
+pca_pipe = pipeline.Pipeline(steps=[('imputation', imp),
+                                    ('scaling', StandardScaler()),
+                                    ('pca', pca)])
+clf_pipe = pipeline.Pipeline(steps=[('imputation', imp),
+                                    ('scaling', StandardScaler()),
+                                    ('pca', pca),
+                                    ('classification',
+                                     RandomForestClassifier(n_estimators=300))])
+pca_pipe.fit(X)
+
+plt.figure(1, figsize=(4, 3))
+plt.clf()
+plt.axes([.2, .2, .7, .7])
+plt.plot(pca.explained_variance_, linewidth=2)
+plt.axis('tight')
+plt.xlabel('n_components')
+plt.ylabel('explained_variance_')
+
+n_components = range(5, 20)
+ns_estimators = [100, 200, 500]
+n_cv_iter = 5
+skf = StratifiedShuffleSplit(y, n_iter=n_cv_iter, test_size=1./n_cv_iter)
+gs_cv = GridSearchCV(clf_pipe, dict(pca__n_components=n_components,
+                                    classification__n_estimators=ns_estimators),
+                     cv=skf)
+gs_cv.fit(X, y)
+print("The best parameters are %s with a score of %0.2f"
+      % (gs_cv.best_params_, gs_cv.best_score_))
+plt.axvline(gs_cv.best_estimator_.named_steps['pca'].n_components,
+            linestyle=':', label='n_components chosen')
+plt.legend(prop=dict(size=12))
+plt.savefig("RndPCA.png", bbox_inches='tight', dpi=500)
+plt.close()
+
+
+# Fit data with best model
+skf = StratifiedShuffleSplit(y, n_iter=1, test_size=0.2)
+for train_index, test_index in skf:
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+best_pipe = gs_cv.best_estimator_
+best_pipe.fit(X_train, y_train)
+y_pred = best_pipe.predict(X_test)
+report = sklearn.metrics.classification_report(y_test, y_pred)
+print("With Randomized PCA :")
+print(report)
+cm = confusion_matrix(y_test, y_pred)
+print_cm_summary(cm)
+
+
+# Using imputation, scaling, PCA & Random Forest
+imp = Imputer(missing_values='NaN', strategy='median', axis=0, verbose=2)
+pca = decomposition.PCA()
+pca_pipe = pipeline.Pipeline(steps=[('imputation', imp),
+                                    ('scaling', StandardScaler()),
+                                    ('pca', pca)])
+clf_pipe = pipeline.Pipeline(steps=[('imputation', imp),
+                                    ('scaling', StandardScaler()),
+                                    ('pca', pca),
+                                    ('classification',
+                                     RandomForestClassifier(n_estimators=300))])
+pca_pipe.fit(X)
+
+plt.figure(1, figsize=(4, 3))
+plt.clf()
+plt.axes([.2, .2, .7, .7])
+plt.plot(pca.explained_variance_, linewidth=2)
+plt.axis('tight')
+plt.xlabel('n_components')
+plt.ylabel('explained_variance_')
+
+n_components = range(5, 20)
+ns_estimators = [100, 300, 500]
+n_cv_iter = 5
+skf = StratifiedShuffleSplit(y, n_iter=n_cv_iter, test_size=1./n_cv_iter)
+gs_cv = GridSearchCV(clf_pipe, dict(pca__n_components=n_components,
+                                    classification__n_estimators=ns_estimators),
+                     cv=skf)
+gs_cv.fit(X, y)
+print("The best parameters are %s with a score of %0.2f"
+      % (gs_cv.best_params_, gs_cv.best_score_))
+plt.axvline(gs_cv.best_estimator_.named_steps['pca'].n_components,
+            linestyle=':', label='n_components chosen')
+plt.legend(prop=dict(size=12))
+plt.savefig("PCA.png", bbox_inches='tight', dpi=500)
+plt.close()
+
+# Fit data with best model
+skf = StratifiedShuffleSplit(y, n_iter=1, test_size=0.2)
+for train_index, test_index in skf:
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+best_pipe = gs_cv.best_estimator_
+best_pipe.fit(X_train, y_train)
+y_pred = best_pipe.predict(X_test)
+report = sklearn.metrics.classification_report(y_test, y_pred)
+print("With PCA :")
+print(report)
+cm = confusion_matrix(y_test, y_pred)
+print_cm_summary(cm)
+
+# Now without PCA
+imp = Imputer(missing_values='NaN', strategy='median', axis=0, verbose=2)
+clf_pipe = pipeline.Pipeline(steps=[('imputation', imp),
+                                    ('scaling', StandardScaler()),
+                                    ('classification',
+                                     RandomForestClassifier())])
+
+ns_estimators = [100, 200, 300, 500]
+n_cv_iter = 5
+skf = StratifiedShuffleSplit(y, n_iter=n_cv_iter, test_size=1./n_cv_iter)
+gs_cv = GridSearchCV(clf_pipe, dict(classification__n_estimators=ns_estimators),
+                     cv=skf)
+gs_cv.fit(X, y)
+print("The best parameters are %s with a score of %0.2f"
+      % (gs_cv.best_params_, gs_cv.best_score_))
+
+# Fit data with best model
+skf = StratifiedShuffleSplit(y, n_iter=1, test_size=0.2)
+for train_index, test_index in skf:
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+best_pipe = gs_cv.best_estimator_
+best_pipe.fit(X_train, y_train)
+y_pred = best_pipe.predict(X_test)
+report = sklearn.metrics.classification_report(y_test, y_pred)
+print("Without Randomized PCA :")
+print(report)
+cm = confusion_matrix(y_test, y_pred)
+print_cm_summary(cm)
